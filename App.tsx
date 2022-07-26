ab9fb514767c9137230a2a26fc39af55ec07e7cc
Pedro Souza <pedroisidorio1@gmail.com>
2022-07-26 19:53:33 -0300

    componentes de input, adição de svg, adição da biblioteca de icones

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider,StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/views/Signin';
+import { Loading } from './src/components/loading';
 
 
 export default function App() {
   const [fontsLoader] = useFonts({ Roboto_400Regular, Roboto_700Bold })
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      <StatusBar   
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      />
+      {fontsLoader ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/views/Signin.tsx
+++ src/views/Signin.tsx
@@ -1,10 +1,29 @@
-import { VStack, Heading } from 'native-base';
-import { getRandomString } from 'native-base/lib/typescript/theme/tools';
-
+import { VStack, Heading, Icon,useTheme } from 'native-base';
+import { Envelope, Key } from "phosphor-react-native";
+import List from '../assets/list-svgrepo-com.svg';
+import { Input } from '../components/input';
 export function SignIn() {
+    const {colors} = useTheme();
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-            <Heading color="gray.100" fontSize="xl" mt={20} mb={6} >Acesse sua conta</Heading>
+
+            <List width="100" height="100" />
+
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6} >
+                Acesse sua conta
+            </Heading>
+
+            <Input
+                mb={4}
+                placeholder="E-mail"
+                InputLeftElement={<Icon ml={4} as={<Envelope color={colors.gray[300]}/>} />}
+                />
+            <Input
+             placeholder="Senha" 
+                InputLeftElement={<Icon ml={4} as={<Key color={colors.gray[300]}/>} />}
+            />
+
+
         </VStack>
     );
-}
+} 
